f23c41305970437dd5d5ea6ef59158c8b2b5a8cd
Bimbok <bimbokmkj@gmail.com>
2026-04-27 22:40:34 +0530

    feat: new-pdf-app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,9 +21,19 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+            isUniversalApk = false
+        }
+    }
+
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
